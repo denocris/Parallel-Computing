c93d2c5b9380693d5037aa4705c2a6dddafd3300
denocris <denobili16@gmail.com>
2016-10-26 16:45:03 +0200

    sistemato

--- D5-exercise/prova.sh
+++ D5-exercise/prova.sh
@@ -2,14 +2,14 @@
 
 ppn=16
 
-for nodes in `seq 1 6`
+for nodes in `seq 1 4`
 do
 
 target=tmp_`echo $nodes`_`echo $ppn`
 cat inizio.sh > $target
-echo "#PBS -l nodes=$nodes:ppn=$ppn" >> $target
+echo "#PBS -l nodes=$nodes:ppn=20" >> $target
 echo "module load openmpi" >> $target
 echo 'cd $PBW_O_WORKDIR' >> $target
-echo "mpirun -np $(($ppn*$nodes)) -npernode $ppn ./a.out" >> $target
+echo "mpirun -np $(($ppn*$nodes)) -npernode $ppn ./a.out 640" >> $target
 qsub $target
 done
