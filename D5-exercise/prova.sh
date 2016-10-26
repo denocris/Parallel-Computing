8bdd17d996e6b96abf357c55243400e8a89c91d9
denocris <denobili16@gmail.com>
2016-10-26 16:08:16 +0200

    modif prova

--- D5-exercise/prova.sh
+++ D5-exercise/prova.sh
@@ -6,10 +6,10 @@ for nodes in `seq 1 6`
 do
 
 target=tmp_`echo $nodes`_`echo $ppn`
-cat inizio > $target
+cat inizio.sh > $target
 echo "#PBS -l nodes=$nodes:ppn=$ppn" >> $target
 echo "module load openmpi" >> $target
 echo 'cd $PBW_O_WORKDIR' >> $target
-echo "mpirun -np $(($ppn*$nodes)) -npernode $ppn ./mio_exec" >> $target
-#qsub $target
+echo "mpirun -np $(($ppn*$nodes)) -npernode $ppn ./a.out" >> $target
+qsub $target
 done
