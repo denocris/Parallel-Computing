159176b9c78a551655f3b2d6d553f7e461d6eea1
denocris <denobili16@gmail.com>
2016-11-02 15:25:39 +0100

    modifica

--- OMP/hello_world_omp.c
+++ OMP/hello_world_omp.c
@@ -7,7 +7,7 @@ int main( int argc, char * argv[]){
 
 #pragma omp parallel
 {
-	printf("I am %d of %d threads",
+	printf("I am %d of %d threads\n",
       omp_get_thread_num(), omp_get_num_threads());
 }
 
