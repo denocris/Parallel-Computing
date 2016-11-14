7cd1f6113c50a08b24d5b68efbccd013b38f47c8
denocris <denobili16@gmail.com>
2016-11-14 22:42:47 +0100

    modif

---
+++ MPIandOpenmp/node_script_c3h.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l walltime=00:10:00
+
+
+# exec_name and output_name must be defined in master_script.sh
+cd $PBS_O_WORKDIR
+module load openmpi
+
+divisors="1 2 4 6 12"
+for i in $divisors
+do
+proc_per_res=$((12/$i))
+export OMP_NUM_THREADS=$i
+time=`mpirun --map-by ppr:$proc_per_res:socket:pe=$i ./$exec_name $mat_size | grep 152`
+echo "$n $i $time" >> $output_name
+done
+
+# the output is printed with a trailing 152, so as to easily discard all lines of warning
