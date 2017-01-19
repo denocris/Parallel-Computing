6fee546bfc5432062b1345d86b2f6e405c9df43f
denocris <denobili16@gmail.com>
2017-01-19 10:07:03 +0100

    modifica

--- Opt_tech/exercise1-matrixmult-blas.cpp
+++ Opt_tech/exercise1-matrixmult-blas.cpp
@@ -13,10 +13,19 @@ double mytime(){
   return v.tv_sec+v.tv_usec/1000000.0;
 }
 
+void matrixmul_naive(double* c,double* a,double* b){
+    for(int i=0; i<4; i++)
+      for(int j=0; j<4; j++)
+        for(int k=0; k<4; k++){
+          c[i*4 + j]+=a[i*4 + k]*b[k*4 + j];
+      }
+}
+
 void matrixmul_mnk(double* c,double* a,double* b){
   cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
 	      mnk, mnk, mnk, 1, a, mnk, b, mnk, 1, c, mnk);
 }
+
 
 void matrixmul_mnk_AN(double* c,double* a,double* b){
     for(int i=0; i<4; i++)
@@ -33,6 +42,7 @@ int main(void){
   double* b= (double*) _mm_malloc(sizeof(double)*size,64);
   double* c= (double*) _mm_malloc(sizeof(double)*size,64);
   double time1,time2;
+  double time1_cblas,time2_cblas;
   double time1_AN,time2_AN;
   for(int i=0;i<size;i++){
     a[i]=rand();
@@ -40,19 +50,39 @@ int main(void){
     c[i]=rand();
   }
 
+  //-------------- NAIVE IMPLEMENTATION ----------
+
   time1=mytime();
+  for(int m=0;m<iter;m++)
+    for(int n=0;n<nmatrices;n++){
+      matrixmul_naive(c, a, b);
+  }
+  time2=mytime();
+
+  printf("matrix C1 = %f \n", c[3]);
+
+
+  //-------------- CBLAS IMPLEMENTATION ----------
+
+  for(int i=0;i<size;i++){
+    c[i]=rand();
+  }
+
+  time1_cblas=mytime();
   for(int n=0;n<iter;n++){
     for(int i=0;i<size;i+=mnk*mnk){
       matrixmul_mnk(&c[i],&a[i],&b[i]);
     }
   }
-  time2=mytime();
+  time2_cblas=mytime();
 
   printf("matrix C1 = %f \n", c[3]);
-   for(int i=0;i<size;i++){
-     c[i]=rand();
-   }
 
+  //-------------- ARRAY NOTATION IMPL ----------
+
+  for(int i=0;i<size;i++){
+    c[i]=rand();
+  }
 
   time1_AN=mytime();
   for(int m=0;m<iter;m++)
@@ -61,11 +91,14 @@ int main(void){
   }
   time2_AN=mytime();
 
-    printf("matrix C2 = %f \n", c[3]);
+  printf("matrix C3 = %f \n", c[3]);
 
+  //------------------------------------------
 
-  printf("time cblas = %f s\n", time2-time1);
+  printf("time naive = %f s\n", time2-time1);
+  printf("time cblas = %f s\n", time2_cblas-time1_cblas);
   printf("time AN = %f s\n", time2_AN-time1_AN);
-  printf("perf cblas = %f GFLOPs\n", (2.0*mnk*mnk*mnk*nmatrices*iter)/(time2-time1)/1000.0/1000.0/1000.0);
+  printf("perf naive = %f GFLOPs\n", (2.0*mnk*mnk*mnk*nmatrices*iter)/(time2-time1)/1000.0/1000.0/1000.0);
+  printf("perf cblas = %f GFLOPs\n", (2.0*mnk*mnk*mnk*nmatrices*iter)/(time2_cblas-time1_cblas)/1000.0/1000.0/1000.0);
   printf("perf AN = %f GFLOPs\n", (2.0*mnk*mnk*mnk*nmatrices*iter)/(time2_AN-time1_AN)/1000.0/1000.0/1000.0);
 }
