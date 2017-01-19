33f3dc73d96a26b6a4528a1e015d1ef099412434
denocris <denobili16@gmail.com>
2017-01-19 14:10:15 +0100

    modifica

--- Opt_tech/exercise1-matrixmult-blas.cpp
+++ Opt_tech/exercise1-matrixmult-blas.cpp
@@ -53,13 +53,14 @@ void matrixmul_mnk_AN(double* c,double* a,double* b){
 
 void matrixmul_intrinsic(double* c, double* a, double* b){
   __m256d a_line, b_line, c_line;
+  c_line = _mm256_load_pd(c);
       for(int i=0;i<mnk*mnk;i+=4){
         for (int j = 0; j < 4; j++) {
             a_line = _mm256_load_pd(&a[j*4]); //a_line = vec4(column(a,j))
             b_line = _mm256_set1_pd(b[i+j]); //b_line = vec4(b[i][j])
             c_line = _mm256_add_pd(_mm256_mul_pd(a_line,b_line), c_line); //r_line += a_line*b_line
         }
-        _mm256_store_pd(&c[i], c_line);// r[i] = r_line
+        _mm256_store_pd(&c[i], c_line);
       }
 
 }
