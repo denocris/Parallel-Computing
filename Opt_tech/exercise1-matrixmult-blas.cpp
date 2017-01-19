7fa15da5967d8d6b4f6d8e033c5d155543cbe7b3
denocris <denobili16@gmail.com>
2017-01-19 12:09:13 +0100

    modifica

--- Opt_tech/exercise1-matrixmult-blas.cpp
+++ Opt_tech/exercise1-matrixmult-blas.cpp
@@ -63,9 +63,9 @@ int main(void){
   double time1_AN,time2_AN;
   double time1_intrinsic,time2_intrinsic;
   for(int i=0;i<size;i++){
-    a[i]=2;
-    b[i]=3;
-    c[i]=rand();
+    a[i]=2.0f;
+    b[i]=3.0f;
+    c[i]=0.0f;
   }
 
   printf("matrix C0 = %f \n", c[3]);
@@ -116,7 +116,7 @@ int main(void){
   //-------------- INTRINSIC IMPLEMENTATION ----------
 
   for(int i=0;i<size;i++){
-    c[i]=rand()/100000000;
+    c[i]=0.0f;
   }
 
   time1_intrinsic=mytime();
